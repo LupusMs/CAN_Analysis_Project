ef2419409d5873724f4e4af002ccfa7da477db90
hazletgathigia <hazletgathigia@gmail.com>
2020-12-11 08:45:16 +0100

    Added the Processed_CSVs folder with the identifier information

--- ID_to_Timelog_Match.py
+++ ID_to_Timelog_Match.py
@@ -17,7 +17,7 @@ class Identifier:
 
     payload_changes = -1
     id_int = 0
-    last_payload = "n"
+    last_payload = "xyz"
     unique_payloads = {}  # or use a set instead
     time_dict = {}
 
@@ -25,30 +25,37 @@ class Identifier:
 ##########################################################################################
 identifier_dict = {}  # a container for all the identifier objects
 timelog_dict = {}
-priority_ids_list = []
 
 # create and specify the necessary paths/folders
+if not os.path.exists(os.path.join("Output_CSVs","Processed_CSVs")):
+    os.mkdir(os.path.join("Output_CSVs","Processed_CSVs"))
+    print("Processed_CSVs directory has been created")
+else:
+    print("Processed_CSVs directory already exists")
+
 tlog_input_path = os.path.join(".", "TimeLog_CSVs", "*.csv")
-output_path = os.path.join(".", "Output_CSVs")
+output_path = os.path.join(".", "Output_CSVs", "Processed_CSVs")
 db_path = os.path.join(".", "Makeshift_DB")
+
 
 # Read from the makeshift database
 with open(os.path.join(db_path, "Identifier_DB_pickle"), "rb") as db:
     identifier_dict = pickle.load(db)
 
 print("0x108 unique payloads are:")
-print(identifier_dict["208"].unique_payloads)
+print(identifier_dict["108"].unique_payloads, end="\n")
 
-print("0x108 time_dict is:")
-print(identifier_dict["208"].time_dict)
+# Write out all the identifiers processed information to individual csvs in the Processed_CSVs folder
+for id in identifier_dict:
+    filename = f"0x{id}.csv" 
 
-with open(os.path.join(output_path, "208_sunroof.csv"), "w") as f:
-    f_writer = csv.writer(f)
-    f_writer.writerow(["Time", "Payload", "Payload_Frequency"])
+    with open(os.path.join(output_path, filename), "w") as f:
+        f_writer = csv.writer(f)
+        f_writer.writerow(["Time", "Payload", "Payload_Occurrences"])
 
-    for k in sorted(identifier_dict["208"].time_dict):
-        v = identifier_dict["208"].time_dict[k]
-        f_writer.writerow([k, v, identifier_dict["208"].unique_payloads[v]])
+        for k in sorted(identifier_dict[id].time_dict):
+            v = identifier_dict[id].time_dict[k]
+            f_writer.writerow([k, v, identifier_dict[id].unique_payloads[v]])
 
 
 
